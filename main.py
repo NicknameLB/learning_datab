0808d16128a20e37524ecd490708f5096ce26ed4
FeodorG <FeodorG2505@mail.ru>
2022-04-21 00:33:40 +0300

    Added sorting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pickle
+import operator
+from datetime import datetime
 
 
 class Train:
@@ -15,35 +17,16 @@ def end():
     print()
 
 
-f = open('trains.txt', 'r', encoding='utf-8')
-dat = []
-while True:
-    x = f.readline()
-    if not x: break
-    x = x.split(',')
-    train = Train()
-    train.wFrom = x[0]
-    train.where = x[1]
-    train.time = x[2]
-    train.tClass = x[3]
-    train.travelTime = x[4]
-    train.cost = x[5]
-    dat.append(train)
-    print(train.wFrom, train.where, train.time, train.tClass, train.travelTime, train.cost)
-
-fout = open('trains.dat', 'wb')
-pickle.dump(dat, fout)
-fout.close()
-
-
 f = open('trains.dat', 'rb')
 newd = pickle.load(f)
 f.close()
 
 while True:
     print('Введите число для выполнения одной из операций')
-    print('1-вывод БД')
-    print('2-выход из программы')
+    print('1-вывод расписания')
+    print('2-запись в файл')
+    print('4-Сортировка')
+    print('0-выход из программы')
     n = int(input())
     if n == 1:
         end()
@@ -51,6 +34,34 @@ while True:
             print(x.wFrom, x.where, x.time, x.tClass, x.travelTime, x.cost)
         end()
     elif n == 2:
+        try:
+            with open(input('Введите название файла: '), 'wb+') as f:
+                pickle.dump(newd, f)
+        except OSError:
+            print('Не удалось создать файл')
+        else:
+            print('Файл успешно создан')
+    elif n == 4:
+        a = []
+        for x in newd:
+            a.append(x)
+        typeS = input(
+            'Введите параметр сортировки (станции отправления, станции назначения, время отправления, время в пути):')
+        if typeS == 'станции отправления':
+            a = sorted(a, key=operator.attrgetter('wFrom'))
+        elif typeS == 'станции назначения':
+            a = sorted(a, key=operator.attrgetter('where'))
+        elif typeS == 'время отправления':
+            a.sort(key=lambda date: datetime.strptime(date.time, '%H:%M'))
+        elif typeS == 'время в пути':
+            a.sort(key=lambda date: datetime.strptime(date.travelTime, '%Hч %Mмин'))
+        end()
+        for x in a:
+            print(x.wFrom, x.where, x.time, x.tClass, x.travelTime, x.cost)
+
+
+
+    elif n == 0:
         print('Выйти? Y/N')
         if input() == 'Y':
             exit()
