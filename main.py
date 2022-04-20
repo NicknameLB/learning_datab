1df951130585678427745084f6f3e8da6b215ab4
NicknameLB <qwer1235qwerr@gmail.com>
2022-04-20 22:36:06 +0300

    big update separated initiation from main to _init.py

--- main.py
+++ main.py
@@ -14,36 +14,15 @@ def end():
     print('-' * 25)
     print()
 
-
-f = open('trains.txt', 'r', encoding='utf-8')
-dat = []
-while True:
-    x = f.readline()
-    if not x: break
-    x = x.split(',')
-    train = Train()
-    train.wFrom = x[0]
-    train.where = x[1]
-    train.time = x[2]
-    train.tClass = x[3]
-    train.travelTime = x[4]
-    train.cost = x[5]
-    dat.append(train)
-    print(train.wFrom, train.where, train.time, train.tClass, train.travelTime, train.cost)
-
-fout = open('trains.dat', 'wb')
-pickle.dump(dat, fout)
-fout.close()
-
-
 f = open('trains.dat', 'rb')
 newd = pickle.load(f)
 f.close()
 
 while True:
     print('Введите число для выполнения одной из операций')
-    print('1-вывод БД')
-    print('2-выход из программы')
+    print('1-вывод расписания')
+    print('2-запись в файл')
+    print('0-выход из программы')
     n = int(input())
     if n == 1:
         end()
@@ -51,6 +30,16 @@ while True:
             print(x.wFrom, x.where, x.time, x.tClass, x.travelTime, x.cost)
         end()
     elif n == 2:
+        try:
+            with open(input('Введите название файла: '), 'wb') as f:
+                newd = pickle.load(f)
+                f.close()
+        except OSError:
+            print('Не удалось создать файл')
+        else:
+            print('Файл успешно создан')
+
+    elif n == 0:
         print('Выйти? Y/N')
         if input() == 'Y':
             exit()
